02460c9bbb14f8876d64131272783b69d5d8ce8d
Noelle Patterson <noellekpatterson@gmail.com>
2018-09-20 18:39:52 -0700

    add generation of inputs for Tukey analysis, for Avg and CV so far

--- tukey_input.py
+++ tukey_input.py
@@ -1,11 +1,22 @@
 import glob
+import numpy as np
 import csv
 from Utils.sortGages import sortGages
 
 files = glob.glob("All-Results/*_annual_result_matrix.csv")
 classes = sortGages(files)
 class_label = []
-metric = []
+Avg = []
+CV = []
 for currentClass, value in classes.items():
-    metric.append(value[i].loc['Avg'])
-    
+    for i, annual in enumerate (value):
+        for index, subyear in enumerate(annual):
+            Avg.append(annual.loc['Avg'][index])
+            CV.append(annual.loc['CV'][index])
+            class_label.append(int(currentClass[-1]))
+
+header = ['group','Avg', 'CV']
+csv_outputs = [class_label, Avg, CV]
+with open('tukey_input.csv', 'w') as csvfile:
+    resultsWriter = csv.writer(csvfile, dialect='excel')
+    resultsWriter.writerows(csv_outputs)
